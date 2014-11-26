ae0408923a0d119589ab2b29c10c195f14548e1f
Shahan Khan <contact@shahan.me>
2014-11-26 14:14:08 -0800

    Added todo list starter

--- js/todo.js
+++ js/todo.js
@@ -1,4 +1,22 @@
 $(function() {
+	// Function to add a todo item
+	var addTodoItem(todo) {
+		// Create a string with html for a list item
+
+
+		// Select the unordered list of todo items
+
+
+		// Append the html to the unordered list of todo items
+		
+
+		// Add a click handler to the delete button in the list item
+		
+
+		// Add a click handler to the input checkbox in the list item
+
+	};
+
 	// Todo Form Submit Handler
 	var handleSubmit = function() {
 		console.log('Form was submitted');
@@ -6,10 +24,12 @@ $(function() {
 		// Get the todo input value
 
 
-		// Create an list element (li) and append to the unordered list (ul)
+		// Add the todo item
 	};
-
 
 	// Setup event handlers
 	$('#add-todo-form').submit(handleSubmit);
+
+	// Load initial page with todo items
+
 });
